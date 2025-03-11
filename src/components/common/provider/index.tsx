0df7d6d1f3181fedc52c751af3daa0226459cc93
박상민 <a01071397193@gmail.com>
2025-03-11 16:40:45 +0900

    Feat :: Style

--- src/components/common/provider/index.tsx
+++ src/components/common/provider/index.tsx
@@ -2,6 +2,12 @@ import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import {ToastContainer} from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+import "src/styles/fonts/_typography.scss";
+import "src/styles/color/_sementic.scss";
+import "src/styles/global.style.scss";
+import "src/styles/shape/_shape.scss";
+import "src/styles/function/_flex.scss";
+
 const queryClient = new QueryClient();
 
 const Provider = () => {
